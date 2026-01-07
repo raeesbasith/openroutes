0290020d6041680d02eed0b7b4fb14c211bf902c
Raees Basith <raeesbasith@gmail.com>
2026-01-07 15:21:18 +0530

    Initial commit

---
+++ OpenRoutes/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for OpenRoutes project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OpenRoutes.settings')
+
+application = get_asgi_application()
