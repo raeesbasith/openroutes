faa14135a141bb725d50f632d34cfffb940f1c40
Raees Basith <raeesbasith@gmail.com>
2026-01-12 15:25:20 +0530

    Add Disability and accessibility Models

--- adminApp/models.py
+++ adminApp/models.py
@@ -9,3 +9,11 @@ class Location(models.Model):
     location_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     district_id = models.ForeignKey(District, on_delete=models.CASCADE)
+
+class Disability(models.Model):
+    disability_id = models.AutoField(primary_key=True)
+    disability_type = models.CharField(max_length=30, unique=True)
+
+class Accessibility(models.Model):
+    accessibility_id = models.AutoField(primary_key=True)
+    accessibility_feature = models.CharField(max_length=50, unique=True)
