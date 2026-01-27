e772bd0de0a0880c43af3f6c3dd64ece70432344
Raees Basith <raeesbasith@gmail.com>
2026-01-27 11:40:00 +0530

    Implement tour packages view and filtered packages view

--- userApp/urls.py
+++ userApp/urls.py
@@ -3,5 +3,9 @@ from . import views
 from .views import *
 
 urlpatterns = [
+
     path('traveller-home/', views.traveller_home, name='traveller_home'),
+    path('tour-packages/', views.tour_packages_view, name='tour_packages'),
+    path('filllocation/', views.filllocation, name='filllocation'),
+    path('tour-detail/<int:tid>/', views.tour_detail, name='tour_detail'),
 ]
--- userApp/views.py
+++ userApp/views.py
@@ -1,5 +1,50 @@
-from django.shortcuts import render
-
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
+from adminApp.models import District, Location, Accessibility
+from operatorApp.models import Tour
 # Create your views here.
 def traveller_home(request):
     return render(request, 'traveller/traveller_home.html')
+
+def tour_packages_view(request):
+    Districts = District.objects.all()
+    accessibilities = Accessibility.objects.all()
+    # Base queryset
+    Tour_list = Tour.objects.all().order_by('-tour_id')
+
+    # Apply filters from GET params
+    district = request.GET.get('district')
+    location = request.GET.get('location')
+    access_list = request.GET.getlist('accessibility')
+
+    if district:
+        Tour_list = Tour_list.filter(location__district__district_id=district)
+
+    if location:
+        Tour_list = Tour_list.filter(location__location_id=location)
+
+    # If accessibilities selected, require tours that have ALL selected features
+    # (chaining filters reduces to intersection)
+    for acc in access_list:
+        if acc:
+            Tour_list = Tour_list.filter(touraccessibility__accessibility__accessibility_id=acc)
+
+    paginator = Paginator(Tour_list.distinct(), 10)  # 10 tours per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'traveller/tour_packages.html', {
+        'Districts': Districts,
+        'accessibilities': accessibilities,
+        'Tour': page_obj
+    })
+
+
+def filllocation(request):
+    did = request.POST.get('did')
+    locations = Location.objects.filter(district_id=did).values('location_id', 'name')
+    return JsonResponse(list(locations), safe=False)
+
+def tour_detail(request, tid):
+    tour = get_object_or_404(Tour, tour_id=tid)
+    return render(request, 'traveller/tour_detail.html', {'tour': tour})
