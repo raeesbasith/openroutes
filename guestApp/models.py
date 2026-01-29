8a7b0e3904b37a1e06cc0c3cb20168fedff90930
Raees Basith <raeesbasith@gmail.com>
2026-01-29 10:54:15 +0530

    Change traveller profile model from userApp to guestApp

--- guestApp/models.py
+++ guestApp/models.py
@@ -19,3 +19,15 @@ class Operator(models.Model):
     status = models.CharField(max_length=20, default='requested')
     license = models.FileField(upload_to='operator_licenses/')
     login = models.ForeignKey(login, on_delete=models.CASCADE)
+
+class TravellerProfile(models.Model):
+    traveller_id = models.AutoField(primary_key=True)
+    traveller_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    phone = models.CharField(max_length=15)
+    address = models.TextField()
+    district = models.ForeignKey(District, on_delete=models.SET_NULL, null=True)
+    city = models.CharField(max_length=100)
+    pincode = models.CharField(max_length=10)
+    login = models.ForeignKey(login, on_delete=models.CASCADE)
+    reg_date = models.DateTimeField(auto_now_add=True)
--- userApp/models.py
+++ userApp/models.py
@@ -2,14 +2,4 @@ from django.db import models
 from guestApp.models import *
 from adminApp.models import *
 # Create your models here.
-class TravellerProfile(models.Model):
-    traveller_id = models.AutoField(primary_key=True)
-    traveller_name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100, unique=True)
-    phone = models.CharField(max_length=15)
-    address = models.TextField()
-    district = models.ForeignKey(District, on_delete=models.SET_NULL, null=True)
-    city = models.CharField(max_length=100)
-    pincode = models.CharField(max_length=10)
-    login = models.ForeignKey(login, on_delete=models.CASCADE)
-    reg_date = models.DateTimeField(auto_now_add=True)
+
